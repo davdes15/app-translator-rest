9d50ed08c3e8a52273fc273c477d101df4eeeaa5
David da Silva <davdes15@gmail.com>
2020-11-09 18:30:26 +0100

    added heroku

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app=express();
 
 app.use(cors());
 app.use( bodyParser.json() ); 
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+app.use(bodyParser.urlencoded({
     extended: true
   })); 
 
@@ -30,3 +30,4 @@ app.post('/translate', function(req,res){
 app.get('/test',function(req,res){
     res.send('a');
 })
+
